ef705a794b888a91ab140eb0646ea7bf2334dc47
Alex Mercer <AlexDMercer13@gmail.com>
2020-10-10 00:38:24 +0300

    Create admin dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,50 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-3">
+				<div class="jumbotron">
+					<h3><span class="badge badge-primary">Категорий 0</span></h3>
+				</div>
+			</div>
+			<div class="col-sm-3">
+				<div class="jumbotron">
+					<h3><span class="badge badge-primary">Материалов 0</span></h3>
+				</div>
+			</div>
+			<div class="col-sm-3">
+				<div class="jumbotron">
+					<h3><span class="badge badge-primary">Посетителей 0</span></h3>
+				</div>
+			</div>
+			<div class="col-sm-3">
+				<div class="jumbotron">
+					<h3><span class="badge badge-primary">Сегодня 0</span></h3>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-6">
+				<a href="#" class="btn btn-block btn-secondary">Создать категорию</a>
+				<a href="#" class="list-group-item">
+					<h4 class="list-group-item-heading">Категория первая</h4>
+					<p class="list-group-item-text">
+						Количество материалов
+					</p>
+				</a>
+			</div>
+			<div class="col-sm-6">
+				<a href="#" class="btn btn-block btn-secondary">Создать материал</a>
+				<a href="#" class="list-group-item">
+					<h4 class="list-group-item-heading">Материал первый</h4>
+					<p class="list-group-item-text">
+						Категория
+					</p>
+				</a>
+			</div>
+		</div>
+
+	</div>
+@endsection
